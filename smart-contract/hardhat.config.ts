dcec86e9ff7daabc2675b957fdcd5a27101a6ca8
15077693d <oscaryiu.lapsang@gmail.com>
2023-07-09 11:08:33 +0800

    feat: add TwoUserMultisig

--- smart-contract/hardhat.config.ts
+++ smart-contract/hardhat.config.ts
@@ -1,24 +1,29 @@
+import { HardhatUserConfig } from "hardhat/config";
+
 import "@matterlabs/hardhat-zksync-deploy";
 import "@matterlabs/hardhat-zksync-solc";
-import "@matterlabs/hardhat-zksync-verify";
 
-module.exports = {
+const config: HardhatUserConfig = {
   zksolc: {
-    version: "latest", // Uses latest available in https://github.com/matter-labs/zksolc-bin/
-    settings: {},
+    version: "latest",
+    settings: {
+      isSystem: true,
+    },
   },
   defaultNetwork: "zkSyncTestnet",
-
   networks: {
-    zkSyncTestnet: {
-      url: "https://testnet.era.zksync.dev",
-      ethNetwork: "https://ethereum-goerli.publicnode.com", // RPC URL of the network (e.g. `https://goerli.infura.io/v3/<API_KEY>`)
+    hardhat: {
       zksync: true,
-      verifyURL:
-        "https://zksync2-testnet-explorer.zksync.dev/contract_verification", // Verification endpoint
+    },
+    zkSyncTestnet: {
+      url: "https://zksync2-testnet.zksync.dev",
+      ethNetwork: "goerli", // Can also be the RPC URL of the network (e.g. `https://goerli.infura.io/v3/<API_KEY>`)
+      zksync: true,
     },
   },
   solidity: {
-    version: "0.8.8",
+    version: "0.8.17",
   },
 };
+
+export default config;
